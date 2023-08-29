e3af966126ff311ad9c388edc0a2ec3939dae6f0
andreas-ahlin <andreas.ahlin12@gmail.com>
2023-08-29 12:12:00 +0200

    Hej

--- Lab1.py
+++ Lab1.py
@@ -6,15 +6,21 @@ class Drama:
 
     def __init__(self, drama):
         self.dramaName = drama[0]
-        self.rating = drama[1]
+        self.rating = float(drama[1])
         self.actors = drama[2]
-        self.viewshipRate = drama[3]
+        self.viewshipRate = float(drama[3])
         self.genre = drama[4]
         self.director = drama[5]
         self.writer = drama[6]
-        self.year = drama[7]
-        self.noOfEpisodes = drama[8]
+        self.year = int(drama[7])
+        self.noOfEpisodes = int(drama[8])
         self.network = drama[9]
+
+    def __str__(self):
+        return 'Namn: ' + self.dramaName + '\n' + 'Rating: ' + str(self.rating) + '\n' + 'Actors: ' + self.actors + '\n' + 'viewshipRate :' + str(self.viewshipRate) + '\n' + 'Genre :' + self.genre + '\n' + 'Director: ' + self.director + '\n' + 'Writer : ' + self.writer + '\n' + 'Year :' + str(self.year) + '\n' + 'NoOfEpisodes :' + str(self.noOfEpisodes) + '\n' + 'Network : ' + self.network
+
+    def __lt__(self, other):
+        return self.rating < other.rating
 
 
 def read_file():
@@ -31,7 +37,8 @@ def read_file():
 
 def main():
     dramalist = read_file()
-    print('hej')
+    print(dramalist[1])
+    print(dramalist[1] < dramalist[2])
 
 
 main()
