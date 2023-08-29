a4d4c23f76d6d9f3aef3aa7f6dbf531e712808c6
andreas-ahlin <andreas.ahlin12@gmail.com>
2023-08-29 20:00:22 +0200

    last update

--- Lab1.py
+++ Lab1.py
@@ -22,6 +22,18 @@ class Drama:
     def __lt__(self, other):
         return self.rating < other.rating
 
+    def check_rating_8(self):
+        if self.rating > 8:
+            return True
+        else:
+            return False
+
+    def check_year_2020(self):
+        if self.year > 2020:
+            return True
+        else:
+            return False
+
 
 def read_file():
     listofdrama = []
@@ -31,14 +43,39 @@ def read_file():
         for line in csvfile:
             newdrama = Drama(line)
             listofdrama.append(newdrama)
-
     return listofdrama
 
 
+def read_file_to_list():
+    with open('kdrama.csv', newline='') as dramafile:
+        csvfile = csv.reader(dramafile, delimiter=',')
+        next(csvfile)
+        listofdrama = list(csvfile)
+    return listofdrama
+
+
+def search_new_drama(listofdramaobject):
+    list2020 = []
+    for item in listofdramaobject:
+        if item.check_year_2020():
+            list2020.append(item)
+    return list2020
+
+
 def main():
-    dramalist = read_file()
-    print(dramalist[1])
-    print(dramalist[1] < dramalist[2])
+    dramalist1 = read_file_to_list()
+    drama1 = Drama(dramalist1[0])
+    drama2 = Drama(dramalist1[1])
+    # print(drama1)
+    # print(drama2)
+    print(drama1 < drama2)
+    print(drama1.check_rating_8())
+    print(drama1.check_year_2020())
+
+    dramaobjectlist = read_file()
+    list2020 = search_new_drama(dramaobjectlist)
+    for item in list2020:
+        print(item.dramaName)
 
 
 main()
